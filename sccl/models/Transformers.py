2c06409d698b8ecf44e57d504c5478d7ff15f8a2
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-20 13:32:19 +0900

    Update Transformers.py

--- sccl/models/Transformers.py
+++ sccl/models/Transformers.py
@@ -1,10 +1,3 @@
-"""
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved
-
-Author: Dejiao Zhang (dejiaoz@amazon.com)
-Date: 02/26/2021
-"""
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
