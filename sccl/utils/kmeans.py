f9abb5c79e5645837a7d8d0b4f6f396fd24a036a
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-20 10:09:06 +0900

    Update kmeans.py

--- sccl/utils/kmeans.py
+++ sccl/utils/kmeans.py
@@ -1,10 +1,3 @@
-"""
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved
-
-Author: Dejiao Zhang (dejiaoz@amazon.com)
-Date: 02/26/2021
-"""
-
 import torch
 import numpy as np
 from utils.metric import Confusion
