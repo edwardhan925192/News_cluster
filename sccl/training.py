707e748b3efd19eddffa6f7fe8aeeae1d815296c
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-20 11:19:50 +0900

    Update training.py

--- sccl/training.py
+++ sccl/training.py
@@ -1,10 +1,3 @@
-"""
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved
-
-Author: Dejiao Zhang (dejiaoz@amazon.com)
-Date: 02/26/2021
-"""
-
 import os
 import time
 import numpy as np
@@ -69,7 +62,6 @@ class SCCLvTrainer(nn.Module):
         
         
     def train_step_virtual(self, input_ids, attention_mask):
-        
         embd1, embd2 = self.model(input_ids, attention_mask, task_type="virtual")
 
         # Instance-CL loss
@@ -93,7 +85,6 @@ class SCCLvTrainer(nn.Module):
     
     
     def train_step_explicit(self, input_ids, attention_mask):
-        
         embd1, embd2, embd3 = self.model(input_ids, attention_mask, task_type="explicit")
 
         # Instance-CL loss
@@ -133,70 +124,6 @@ class SCCLvTrainer(nn.Module):
 
             if (self.args.print_freq>0) and ((i%self.args.print_freq==0) or (i==self.args.max_iter)):
                 statistics_log(self.args.tensorboard, losses=losses, global_step=i)
-                self.evaluate_embedding(i)
-                self.model.train()
+                # Removed the evaluation embedding
 
-        return None   
-
-    
-    def evaluate_embedding(self, step):
-        dataloader = unshuffle_loader(self.args)
-        print('---- {} evaluation batches ----'.format(len(dataloader)))
-        
-        self.model.eval()
-        for i, batch in enumerate(dataloader):
-            with torch.no_grad():
-                text, label = batch['text'], batch['label'] 
-                feat = self.get_batch_token(text)
-                embeddings = self.model(feat['input_ids'].cuda(), feat['attention_mask'].cuda(), task_type="evaluate")
-
-                model_prob = self.model.get_cluster_prob(embeddings)
-                if i == 0:
-                    all_labels = label
-                    all_embeddings = embeddings.detach()
-                    all_prob = model_prob
-                else:
-                    all_labels = torch.cat((all_labels, label), dim=0)
-                    all_embeddings = torch.cat((all_embeddings, embeddings.detach()), dim=0)
-                    all_prob = torch.cat((all_prob, model_prob), dim=0)
-                    
-        # Initialize confusion matrices
-        confusion, confusion_model = Confusion(self.args.num_classes), Confusion(self.args.num_classes)
-        
-        all_pred = all_prob.max(1)[1]
-        confusion_model.add(all_pred, all_labels)
-        confusion_model.optimal_assignment(self.args.num_classes)
-        acc_model = confusion_model.acc()
-
-        kmeans = cluster.KMeans(n_clusters=self.args.num_classes, random_state=self.args.seed)
-        embeddings = all_embeddings.cpu().numpy()
-        kmeans.fit(embeddings)
-        pred_labels = torch.tensor(kmeans.labels_.astype(np.int))
-        
-        # clustering accuracy 
-        confusion.add(pred_labels, all_labels)
-        confusion.optimal_assignment(self.args.num_classes)
-        acc = confusion.acc()
-
-        ressave = {"acc":acc, "acc_model":acc_model}
-        ressave.update(confusion.clusterscores())
-        for key, val in ressave.items():
-            self.args.tensorboard.add_scalar('Test/{}'.format(key), val, step)
-            
-        np.save(self.args.resPath + 'acc_{}.npy'.format(step), ressave)
-        np.save(self.args.resPath + 'scores_{}.npy'.format(step), confusion.clusterscores())
-        np.save(self.args.resPath + 'mscores_{}.npy'.format(step), confusion_model.clusterscores())
-        # np.save(self.args.resPath + 'mpredlabels_{}.npy'.format(step), all_pred.cpu().numpy())
-        # np.save(self.args.resPath + 'predlabels_{}.npy'.format(step), pred_labels.cpu().numpy())
-        # np.save(self.args.resPath + 'embeddings_{}.npy'.format(step), embeddings)
-        # np.save(self.args.resPath + 'labels_{}.npy'.format(step), all_labels.cpu())
-
-        print('[Representation] Clustering scores:',confusion.clusterscores()) 
-        print('[Representation] ACC: {:.3f}'.format(acc)) 
-        print('[Model] Clustering scores:',confusion_model.clusterscores()) 
-        print('[Model] ACC: {:.3f}'.format(acc_model))
         return None
-
-
-
-             
