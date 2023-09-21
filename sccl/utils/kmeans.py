34d29b58fde147b041c4a3c3678d20256e89a866
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-21 13:17:53 +0900

    Update kmeans.py

--- sccl/utils/kmeans.py
+++ sccl/utils/kmeans.py
@@ -4,8 +4,12 @@ from utils.metric import Confusion
 from sklearn.cluster import KMeans
 
 
-def get_mean_embeddings(bert, input_ids, attention_mask):
-        bert_output = bert.forward(input_ids=input_ids, attention_mask=attention_mask)
+def get_embeddings(bert, input_ids, attention_mask, use_cls=False):
+    bert_output = bert.forward(input_ids=input_ids, attention_mask=attention_mask)
+    
+    if use_cls:
+        return bert_output[0][:, 0, :]
+    else:
         attention_mask = attention_mask.unsqueeze(-1)
         mean_output = torch.sum(bert_output[0]*attention_mask, dim=1) / torch.sum(attention_mask, dim=1)
         return mean_output
@@ -22,12 +26,12 @@ def get_batch_token(tokenizer, text, max_length):
     return token_feat
 
 
-def get_kmeans_centers(bert, tokenizer, train_loader, num_classes, max_length):
+def get_kmeans_centers(bert, tokenizer, train_loader, num_classes, max_length, use_cls=False):
     for i, batch in enumerate(train_loader):
 
         text = batch['text']
         tokenized_features = get_batch_token(tokenizer, text, max_length)
-        corpus_embeddings = get_mean_embeddings(bert, **tokenized_features)
+        corpus_embeddings = get_embeddings(bert, use_cls=use_cls, **tokenized_features)
         
         if i == 0:
             all_embeddings = corpus_embeddings.detach().numpy()
@@ -43,6 +47,7 @@ def get_kmeans_centers(bert, tokenizer, train_loader, num_classes, max_length):
     print("Centers shape:", clustering_model.cluster_centers_.shape)
     
     return clustering_model.cluster_centers_
+
 
 
 
