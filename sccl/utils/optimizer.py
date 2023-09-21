26216aea49ce169241e1bea8d29b4da5789ead32
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-21 11:33:26 +0900

    Update optimizer.py

--- sccl/utils/optimizer.py
+++ sccl/utils/optimizer.py
@@ -1,10 +1,3 @@
-"""
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved
-
-Author: Dejiao Zhang (dejiaoz@amazon.com)
-Date: 02/26/2021
-"""
-
 import os
 import torch 
 from transformers import get_linear_schedule_with_warmup
@@ -13,6 +6,7 @@ from sentence_transformers import SentenceTransformer
 
 BERT_CLASS = {
     "distilbert": 'distilbert-base-uncased', 
+    "deberta": "microsoft/deberta-base"
 }
 
 SBERT_CLASS = {
@@ -32,13 +26,19 @@ def get_optimizer(model, args):
     return optimizer 
     
 
-def get_bert(args):
-    
+def get_bert(args):    
     if args.use_pretrain == "SBERT":
         bert_model = get_sbert(args)
         tokenizer = bert_model[0].tokenizer
         model = bert_model[0].auto_model
         print("..... loading Sentence-BERT !!!")
+        
+    elif args.bert.lower() == "deberta":  
+        config = AutoConfig.from_pretrained("microsoft/deberta-base")  
+        model = AutoModel.from_pretrained("microsoft/deberta-base", config=config)  
+        tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")  
+        print("..... loading DeBERTa !!!")  
+        
     else:
         config = AutoConfig.from_pretrained(BERT_CLASS[args.bert])
         model = AutoModel.from_pretrained(BERT_CLASS[args.bert], config=config)
